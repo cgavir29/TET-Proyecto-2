e40e49e470783e66058adff508b8f18393223247
cgavir29 <35697795+cgavir29@users.noreply.github.com>
2020-03-13 15:56:49 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-eval sudo apt-get update 
-eval sudo apt-get upgrade -y
-eval sudo apt-get install docker-compose -y
-eval sudo git clone https://github.com/cgavir29/TET-Proyecto-2 wordpress 
-eval sudo cd wordpress 
-eval sudo docker-compose up -d
+sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get install docker-compose -y
+sudo git clone https://github.com/cgavir29/TET-Proyecto-2 wordpress 
+sudo cd wordpress 
+sudo docker-compose up -d
