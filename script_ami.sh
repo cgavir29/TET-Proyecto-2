d73b1242bd8064db8b9d2a79f74deaca46e82d83
sarredondq <35717046+sarredondq@users.noreply.github.com>
2020-03-21 09:19:02 -0500

    Update script_ami.sh

--- script_ami.sh
+++ script_ami.sh
@@ -4,8 +4,8 @@ yum update -y
 yum install docker git -y
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
-service docker start
-service docker enable
+systemctl start docker
+systemctl enable docker
 git clone https://github.com/cgavir29/TET-Proyecto-2 wordpress 
 cd wordpress 
 docker-compose up -d
